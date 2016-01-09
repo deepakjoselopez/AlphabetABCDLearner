972b066ab26fbbd1729645930fd1b36783a4fda7
deepakjoselopez <deepak_net@yahoo.com>
2016-01-09 17:53:23 -0600

    Chapter01

---
+++ DataModal.js
@@ -0,0 +1,36 @@
+var alpList = [
+    {"alphabet":"A","keyWords":['Apple','Aeroplane']},
+    {"alphabet":"B","keyWords":['Butterfly','Book']},
+    {"alphabet":"C","keyWords":['Cat','Car']},
+    {"alphabet":"D","keyWords":['Dog','Daddy']},
+    
+    {"alphabet":"E","keyWords":['Elephant','Egg']},
+    {"alphabet":"F","keyWords":['Fox','Food']},
+    {"alphabet":"G","keyWords":['Giraff','Girl']},
+    {"alphabet":"H","keyWords":['Home','Horse']},
+    
+    {"alphabet":"I","keyWords":['Igloo','Icecream']},
+    {"alphabet":"J","keyWords":['Jelly','Joker']},
+    {"alphabet":"K","keyWords":['Kite','Kangaroo']},
+    {"alphabet":"L","keyWords":['Lion','Lemon']},
+    
+    {"alphabet":"M","keyWords":['Monkey','Money']},
+    {"alphabet":"N","keyWords":['Nest','Notebook']},
+    {"alphabet":"O","keyWords":['Octopus','Olivia']},
+    {"alphabet":"P","keyWords":['Puppy','Pizza']},
+    
+    {"alphabet":"Q","keyWords":['Queen','Quarter']},
+    {"alphabet":"R","keyWords":['Rabbit','Ribbon']},
+    {"alphabet":"S","keyWords":['Sun','Shell']},
+    {"alphabet":"T","keyWords":['Tiger','Turtle']},
+    
+    {"alphabet":"U","keyWords":['Umbrella','Underwear']},
+    {"alphabet":"V","keyWords":['Violin','Van']},
+    {"alphabet":"W","keyWords":['Whale','Water']},
+    {"alphabet":"X","keyWords":['XRay','Xylophone']},
+    
+    
+    
+    {"alphabet":"Y","keyWords":['Yoyo','Yogurt']},
+    {"alphabet":"Z","keyWords":['Zebra','Zipper']}
+];
